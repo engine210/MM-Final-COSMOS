d6f8006fb7cc1804dc680a0bc9e78a910ea511e0
Steven Wang <engine210@gmail.com>
2021-06-19 23:34:13 +0800

    finish first version runnable code

--- evaluate_ooc_st.py
+++ evaluate_ooc_st.py
@@ -1,12 +1,14 @@
 """ Evaluation script to evaluate Out-of-Context Detection Accuracy"""
 
-import cv2
 import os
+from io import BytesIO
+from PIL import Image
 from utils.config import *
 from utils.common_utils import read_json_data
 from utils.eval_utils import is_bbox_overlap, top_bbox_from_scores
 from sentence_transformers import SentenceTransformer, util
 
+model = SentenceTransformer('clip-ViT-B-32')
 
 def get_scores(v_data):
     """
@@ -19,8 +21,44 @@ def get_scores(v_data):
             score_c1 (float): Score for the first caption associated with the image
             score_c2 (float): Score for the second caption associated with the image
     """
-    
+    img_path = os.path.join(DATA_DIR, v_data["img_local_path"])
+    img = Image.open(img_path)
+    img_shape = img.size
 
+    bbox_list = v_data['maskrcnn_bboxes']
+    bbox_list.append([0, 0, img_shape[0], img_shape[1]])  # For entire image (global context)
+
+    cap1 = v_data['caption1_modified']
+    cap2 = v_data['caption2_modified']
+    # cap1 = v_data['caption1']
+    # cap2 = v_data['caption2']
+
+    # print(cap1)
+    # print(cap2)
+
+    score_c1 = []
+    score_c2 = []
+
+    for i, box in enumerate(bbox_list):
+        box = list(map(int, box))
+        if box[0] == box[2]: box[2] += 1
+        if box[1] == box[3]: box[3] += 1
+
+        crop_img = img.crop(box)
+        b = BytesIO()
+        crop_img.save(b,format="jpeg")
+        crop_img = Image.open(b)
+        # crop_img.save('tmp'+str(i)+'.jpg')
+        crop_img_emb = model.encode(crop_img)
+        text_emb = model.encode([cap1, cap2])
+
+        # #Compute cosine similarities 
+        cos_scores = util.cos_sim(crop_img_emb, text_emb)
+        # print(i, cos_scores.numpy()[0])
+        score_c1.append(cos_scores.numpy()[0][0])
+        score_c2.append(cos_scores.numpy()[0][1])
+
+    print(v_data["img_local_path"], score_c1.index(max(score_c1)), score_c2.index(max(score_c2)))
     return score_c1, score_c2
 
 
@@ -60,25 +98,30 @@ if __name__ == "__main__":
     test_samples = read_json_data(os.path.join(DATA_DIR, 'mmsys_anns', 'public_test_mmsys_final.json'))
     ours_correct = 0
     lang_correct = 0
+    ooc_to_nooc = 0
+    nooc_to_ooc = 0
 
     for i, v_data in enumerate(test_samples):
         actual_context = int(v_data['context_label'])
         
-        #language_context = 0 if float(v_data['bert_base_score']) >= textual_sim_threshold else 1
-
-        language_model = SentenceTransformer('stsb-mpnet-base-v2')
-        cap1_embedding = language_model.encode(v_data['caption1'])
-        cap2_embedding = language_model.encode(v_data['caption2'])
-        sim = util.pytorch_cos_sim(cap1_embedding, cap2_embedding)
-        language_context = 0 if sim >= textual_sim_threshold else 1
+        language_context = 0 if float(v_data['bert_base_score']) >= textual_sim_threshold else 1
 
         pred_context = evaluate_context_with_bbox_overlap(v_data)
 
         if pred_context == actual_context:
             ours_correct += 1
+        else:
+            if actual_context == 0:
+                nooc_to_ooc += 1
+            else:
+                ooc_to_nooc += 1
+                
 
         if language_context == actual_context:
             lang_correct += 1
 
     print("Cosmos Accuracy", ours_correct / len(test_samples))
     print("Language Baseline Accuracy", lang_correct / len(test_samples))
+    print("Total Data Size,", len(test_samples))
+    print("OOC to NOOC,", ooc_to_nooc)
+    print("NOOC to OOC,", nooc_to_ooc)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,3 @@
 from .common_utils import *
 from .config import *
-from .dataset import *
-from .dataset_utils import *
 from .eval_utils import *
-from .img_model_utils import *
-from .text_utils import *
